a4024d194020e727917a7928778dd0af6558b6c8
ilick888 <ilick888@gmail.com>
2020-06-08 23:28:22 +0900

    reducer簡易化

--- src/components/comment_post.js
+++ src/components/comment_post.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
 import { connect } from 'react-redux';
 import { TextField, Button } from '@material-ui/core';
 import Title from './title';
--- src/reducers/comment.js
+++ src/reducers/comment.js
@@ -1,18 +1,11 @@
 import { READ_COMMENT,CREATE_COMMENT } from "../actions/action_comment";
 
-export default (comments = {}, action) => {
+export default (comments = [], action) => {
     switch(action.type){
         case READ_COMMENT:
             return action.response.data
         case CREATE_COMMENT:
-            const data = Object.entries({...comments}).map(([key, value]) => ({
-                'id': value.id, 
-                'comment': value.comment,
-                'created_at' : value.created_at,
-                'stock' : value.stock,
-            }))
-            data.unshift(action.response.data)
-            return data
+            return [action.response.data, ...comments]
         default:
             return comments
     }
