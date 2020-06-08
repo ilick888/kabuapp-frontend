f52e62ea1d16bec68998b5847a868224ab72f9b2
ilick888 <ilick888@gmail.com>
2020-06-08 23:20:47 +0900

    投稿反映ライフサイクルメソッド修正

--- src/reducers/comment.js
+++ src/reducers/comment.js
@@ -5,7 +5,14 @@ export default (comments = {}, action) => {
         case READ_COMMENT:
             return action.response.data
         case CREATE_COMMENT:
-            return { ...comments}
+            const data = Object.entries({...comments}).map(([key, value]) => ({
+                'id': value.id, 
+                'comment': value.comment,
+                'created_at' : value.created_at,
+                'stock' : value.stock,
+            }))
+            data.unshift(action.response.data)
+            return data
         default:
             return comments
     }
